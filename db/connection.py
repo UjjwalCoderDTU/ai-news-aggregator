815f3078137a307b0fc667a288e1eb1d3e996ebd
Ujjwal <ujjwal@Ujjwals-MacBook-Air.local>
2026-04-17 01:27:20 +0530

    fix database connection to use DATABASE_URL

--- db/connection.py
+++ db/connection.py
@@ -5,28 +5,19 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DB_HOST = os.getenv("DB_HOST")
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_NAME = os.getenv("DB_NAME")
-DB_PORT = os.getenv("DB_PORT")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-# Used for PostgreSql locally
-# # Fix password issue (@ → %40)
-# from urllib.parse import quote_plus
-# encoded_password = quote_plus(DB_PASSWORD)
-
-# Create engine
-DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-
-
-engine = create_engine(DATABASE_URL ,pool_pre_ping=True)
+engine = create_engine(DATABASE_URL,pool_pre_ping=True)
 
 SessionLocal = sessionmaker(bind=engine)
 
 Base = declarative_base()
 
-
 def init_db():
     Base.metadata.create_all(bind=engine)
 
+
+# Used for PostgreSql locally
+# # Fix password issue (@ → %40)
+# from urllib.parse import quote_plus
+# encoded_password = quote_plus(DB_PASSWORD)
